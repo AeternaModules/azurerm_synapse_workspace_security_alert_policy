9c279fe3c17af34c586fffa07a7e43a593e131f2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,34 @@
-output "synapse_workspace_security_alert_policies" {
-  description = "All synapse_workspace_security_alert_policy resources"
-  value       = azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies
-  sensitive   = true
-}
 output "synapse_workspace_security_alert_policies_disabled_alerts" {
-  description = "List of disabled_alerts values across all synapse_workspace_security_alert_policies"
-  value       = [for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : v.disabled_alerts]
+  description = "Map of disabled_alerts values across all synapse_workspace_security_alert_policies, keyed the same as var.synapse_workspace_security_alert_policies"
+  value       = { for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : k => v.disabled_alerts }
 }
 output "synapse_workspace_security_alert_policies_email_account_admins_enabled" {
-  description = "List of email_account_admins_enabled values across all synapse_workspace_security_alert_policies"
-  value       = [for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : v.email_account_admins_enabled]
+  description = "Map of email_account_admins_enabled values across all synapse_workspace_security_alert_policies, keyed the same as var.synapse_workspace_security_alert_policies"
+  value       = { for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : k => v.email_account_admins_enabled }
 }
 output "synapse_workspace_security_alert_policies_email_addresses" {
-  description = "List of email_addresses values across all synapse_workspace_security_alert_policies"
-  value       = [for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : v.email_addresses]
+  description = "Map of email_addresses values across all synapse_workspace_security_alert_policies, keyed the same as var.synapse_workspace_security_alert_policies"
+  value       = { for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : k => v.email_addresses }
 }
 output "synapse_workspace_security_alert_policies_policy_state" {
-  description = "List of policy_state values across all synapse_workspace_security_alert_policies"
-  value       = [for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : v.policy_state]
+  description = "Map of policy_state values across all synapse_workspace_security_alert_policies, keyed the same as var.synapse_workspace_security_alert_policies"
+  value       = { for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : k => v.policy_state }
 }
 output "synapse_workspace_security_alert_policies_retention_days" {
-  description = "List of retention_days values across all synapse_workspace_security_alert_policies"
-  value       = [for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : v.retention_days]
+  description = "Map of retention_days values across all synapse_workspace_security_alert_policies, keyed the same as var.synapse_workspace_security_alert_policies"
+  value       = { for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : k => v.retention_days }
 }
 output "synapse_workspace_security_alert_policies_storage_account_access_key" {
-  description = "List of storage_account_access_key values across all synapse_workspace_security_alert_policies"
-  value       = [for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : v.storage_account_access_key]
+  description = "Map of storage_account_access_key values across all synapse_workspace_security_alert_policies, keyed the same as var.synapse_workspace_security_alert_policies"
+  value       = { for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : k => v.storage_account_access_key }
   sensitive   = true
 }
 output "synapse_workspace_security_alert_policies_storage_endpoint" {
-  description = "List of storage_endpoint values across all synapse_workspace_security_alert_policies"
-  value       = [for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : v.storage_endpoint]
+  description = "Map of storage_endpoint values across all synapse_workspace_security_alert_policies, keyed the same as var.synapse_workspace_security_alert_policies"
+  value       = { for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : k => v.storage_endpoint }
 }
 output "synapse_workspace_security_alert_policies_synapse_workspace_id" {
-  description = "List of synapse_workspace_id values across all synapse_workspace_security_alert_policies"
-  value       = [for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : v.synapse_workspace_id]
+  description = "Map of synapse_workspace_id values across all synapse_workspace_security_alert_policies, keyed the same as var.synapse_workspace_security_alert_policies"
+  value       = { for k, v in azurerm_synapse_workspace_security_alert_policy.synapse_workspace_security_alert_policies : k => v.synapse_workspace_id }
 }
 
